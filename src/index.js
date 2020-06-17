c77c3628b2ffe301f69b68d4a32ede5004fa4ecb
Vorák Gergő <vorakgeri@gmail.com>
2020-06-17 17:43:24 +0200

    3.11 routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
- import React from 'react';
+ import React from 'react'
  import {render} from 'react-dom'
- import StorePicker from './components/StorePicker'
- import App from './components/App'
+ import Router from "./components/Router"
  import "./css/style.css";
 
 
- render(<App/>, document.getElementById('main'));
+ render(<Router/>, document.getElementById('main'));
